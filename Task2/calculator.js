57a450f77688d196c41c927b0e607eb7c267b73a
GaneshMajagi <majagiganesh@gmail.com>
2025-08-20 12:45:08 +0530

    Add files via upload

---
+++ Task2/calculator.js
@@ -0,0 +1,25 @@
+document.getElementById("calculateBtn").addEventListener("click", function() {
+      const num1 = parseFloat(document.getElementById("num1").value);
+      const num2 = parseFloat(document.getElementById("num2").value);
+      const operator = document.getElementById("operator").value.trim();
+      let result;
+
+      switch (operator) {
+        case '+':
+          result = num1 + num2;
+          break;
+        case '-':
+          result = num1 - num2;
+          break;
+        case '*':
+          result = num1 * num2;
+          break;
+        case '/':
+          result = num2 !== 0 ? num1 / num2 : 'Cannot divide by zero';
+          break;
+        default:
+          result = 'Invalid operator';
+      }
+
+      document.getElementById("result").textContent = result;
+    });
